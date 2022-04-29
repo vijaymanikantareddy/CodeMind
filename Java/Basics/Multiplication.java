9479a18f8cc9965dc892e08423c895a07b6762a9
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-04-29 21:45:54 +0530

    Create Multiplication.java

---
+++ Java/Basics/Multiplication.java
@@ -0,0 +1,11 @@
+import java.io.*;
+class Sample{
+    public static void main(String args[])throws Exception
+    {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int a, b;
+        a = Integer.parseInt(br.readLine());
+        b = Integer.parseInt(br.readLine());
+        System.out.format("%d",a*b);
+    }
+}
